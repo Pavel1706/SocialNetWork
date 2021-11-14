4dfc31efa1c3cbf07e678345b66af82a74b0539d
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-14 19:32:28 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+import ReactDOM from 'react-dom';
+import {SamuraiJSApp} from "./App";
+//test commit
+ReactDOM.render(
+    <SamuraiJSApp/>,
+    document.getElementById('root')
+)
+
+
+
+// <BrowserRouter>
+// <Provider store={store}>
+//     <App/>
+//     </Provider>
+// </BrowserRouter>,
