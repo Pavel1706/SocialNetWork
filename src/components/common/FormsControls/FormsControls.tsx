f8d8aeb200b004d13cc99a073ba66e1a3c2b35cd
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-22 10:28:31 +0300

    feat: refactoring FormControls

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import styles from './FormControls.module.css'
-import {maxLengthCreator, requiredField} from "../../../utils/validators/validators";
 import {Field} from "redux-form";
 
 
-//  const FormControl  = ({...props})=> {
-//     let input = props.input
-//     let meta = props.meta
-//     const hasError = meta.touched && meta.error
-//     return (
-//         <div className={styles.formControl + " " + (hasError ? styles.error: '')} >
-//             <div>
-//                 {props.children}
-//             </div>
-//             { hasError && <span> {meta.error}</span>}
-//         </div>
-//     )
-// }
 export const Textarea = ({...props})=> {
     let input = props.input
     let meta = props.meta
@@ -46,18 +32,7 @@ export const Input = ({...props})=> {
         </div>
     )
 }
-// export const requiredField = (value:string)=> {
-//     if (value) return undefined;
-//     return 'Field is required'
-// }
-//
-//
-//
-// export const maxLengthCreator =(maxLength: number) => (value: string) => {
-//     if (value && value.length > maxLength) return `Max length is ${maxLength} symbols`;
-//     return undefined
-// }
-// const maxLength30 = maxLengthCreator(30)
+
 
 export const CreateField = (placeholder: string, name:string,
                             validate:[requiredField:any,
