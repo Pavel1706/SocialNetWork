3faa330495ee65fd6b61be31ec2eff3455464318
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-22 10:29:01 +0300

    feat: update Paginator.tsx

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "../../common/Paginator/Paginator.module.css";
+import cn from "classnames";
+
 
 type PaginatorType = {
     pageSize: number
     totalUsersCount: number
     currentPage: number
     onPageChanged: (value: number) => void
+    portionSize: number
 
 }
-
 
 export let Paginator = (props: PaginatorType) => {
 
@@ -18,13 +20,33 @@ export let Paginator = (props: PaginatorType) => {
         pages.push(i)
     }
 
-    return   <div>
-            {pages.map(t => {
-                return <span className={props.currentPage === t ? styles.selectedPage : ''}
-                             onClick={() => {
-                                 props.onPageChanged(t)
-                             }}>{t}</span>
+    let portionSize = props.portionSize
+
+    let portionCount = Math.ceil(pagesCount / portionSize)
+    let [portionNumber, setPortionNumber] = useState(1)
+    let leftPortionNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize
+
+
+    return <div className={styles.paginator}>
+        {portionNumber > 1 &&
+        <button className={styles.arrowLeft} onClick={() => {
+            setPortionNumber(portionNumber - 1)
+        }}></button>}
+        {pages.filter(p => p >= leftPortionNumber && p <= rightPortionPageNumber)
+            .map((p) => {
+                return <span className={cn({
+                    [styles.selectedPage]: props.currentPage === p
+                }, styles.pageNumber)}
+                             key={p}
+                             onClick={(e) => {
+                                 props.onPageChanged(p)
+                             }}>{p}</span>
             })}
-        </div>
+        {portionCount > portionNumber &&
+        <button className={styles.arrowRight} onClick={() => {
+            setPortionNumber(portionNumber + 1)
+        }}></button>}
+    </div>
 
 }
