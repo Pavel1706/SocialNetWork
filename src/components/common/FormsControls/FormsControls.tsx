6eb6351d2482c51b669b2fde892cd9403885b579
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-23 23:09:41 +0300

    feat: update FormsControls

--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -1,42 +1,73 @@
 import React from 'react';
 import styles from './FormControls.module.css'
-import {Field} from "redux-form";
+import {Field, WrappedFieldMetaProps, WrappedFieldProps} from "redux-form";
+import {FieldValidatorType, requiredField} from '../../../utils/validators/validators';
 
 
-export const Textarea = ({...props})=> {
-    let input = props.input
-    let meta = props.meta
-    const hasError = meta.touched && meta.error
-    return (
-        <div className={styles.formControl + " " + (hasError ? styles.error: '')} >
-            <div>
-                <textarea {...props} {...input} {...meta}/>
+type FormControlPropsType = {
+    meta: WrappedFieldMetaProps
+}
+
+const FormControl: React.FC<FormControlPropsType> =
+    ({meta: {touched, error}, children}) => {
+        const hasError = touched && error;
+        return (
+            <div className={styles.formControl + '' + (hasError ? styles.error : "")}>
+                <div>
+                    {children}
+                </div>
+                {hasError && <span>{error}</span>}
             </div>
-            { hasError && <span> {meta.error}</span>}
-        </div>
-    )
+
+        )
+    }
+
+// (
+//     <div className={styles.formControl + " " + (hasError ? styles.error: '')} >
+//         <div>
+//             <textarea {...props} {...input} {...meta}/>
+//         </div>
+//         { hasError && <span> {meta.error}</span>}
+//     </div>
+// )
+export const Textarea: React.FC<WrappedFieldProps> = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormControl {...props}><textarea{...input}{...restProps}/></FormControl>
 }
 
 
-export const Input = ({...props})=> {
+export const Input: React.FC<WrappedFieldProps> = (props) => {
 
-    let input = props.input
-    let meta = props.meta
-    const hasError = meta.touched && meta.error
-    return (
-        <div className={styles.formControl + " " + (hasError ? styles.error: '')} >
-            <div>
-                <input {...props} {...input} {...meta}/>
-            </div>
-            { hasError && <span> {meta.error}</span>}
-        </div>
-    )
+    // let input = props.input
+    // let meta = props.meta
+    // const hasError = meta.touched && meta.error
+    const {input, meta, ...restProps} = props
+
+    return <FormControl {...props}><input{...input}{...restProps}/></FormControl>
+    // (
+    //     <div className={styles.formControl + " " + (hasError ? styles.error: '')} >
+    //         <div>
+    //             <input {...props} {...input} {...meta}/>
+    //         </div>
+    //         { hasError && <span> {meta.error}</span>}
+    //     </div>
+    // )
 }
 
 
-export const CreateField = (placeholder: string, name:string,
-                            validate:[requiredField:any,
-                                maxLength30:any],component:any)=>
-    <Field placeholder={placeholder} name={name}
-                                       validate={[validate]}
-                                       component={component}/>
+export function createField<FormKeysType extends string>(placeholder: string | undefined,
+                                                         name: FormKeysType,
+                                                         validators: Array<FieldValidatorType>,
+                                                         component: React.FC<WrappedFieldProps>,
+                                                         props = {}, text = '') {
+    return <div>
+        <Field placeholder={placeholder}
+               name={name}
+               validate={validators}
+               component={component}
+               {...props}
+        /> {text}
+    </div>
+}
+
+export type GetStringKeys<T> = Extract<keyof T, string>
