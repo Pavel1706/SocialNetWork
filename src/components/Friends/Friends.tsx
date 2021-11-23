3ca6211c5e23ef88b5c602070edc2f473c13ce36
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-23 23:10:30 +0300

    feat: update Friends.tsx

--- src/components/Friends/Friends.tsx
+++ src/components/Friends/Friends.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
-
 import style from './Friend/friends.module.css'
 import {ConversationType} from "../../Redux/dialogsReducer";
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../Redux/reduxStore";
 
 
-type ConversationArrayType = {
-    dialogs: ConversationType[]
-}
 
 
-export const Friends =(props:ConversationArrayType)=>{
-
+export const Friends =()=>{
+    let dialogsPage = useSelector<AppStateType,ConversationType[]>(state => state.dialogsPage.dialogs)
     return(
         <span className={style.avatar} >
 
-            {props.dialogs.map(t=>  <div key={t.id}> <img alt={'foto'} className={style.avatar} src={t.foto} />{t.name}</div> )}
+            {dialogsPage.map(t=>  <div key={t.id}> <img alt={'foto'} className={style.avatar} src={t.foto} />{t.name}</div> )}
 
         </span>
 
