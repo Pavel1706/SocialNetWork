ff416df67e4c6d45f6edb2802ac9df6018f15fe7
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-27 01:00:06 +0300

    feat: change Post.tsx

--- src/components/Profile/myPosts/Post/Post.tsx
+++ src/components/Profile/myPosts/Post/Post.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import k from './Post.module.css';
+import React, {useState} from 'react';
+import style from './Post.module.css';
 
 
-export type Dreamtype ={
-
+export type Posttype ={
     message : string
     like: number
 }
 
-export const Post = (props:Dreamtype) => {
+export const Post = (props:Posttype) => {
+    const [like, setLike]=useState(props.like)
+
+
+
     return (
-        <div className={k.since}>
+        <div className={style.since}>
             <img alt={'foto'}
                 src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6C1rS1qYf8Z6en9SWnpaIOarrDPK_G66T0Q&usqp=CAU'/>
             {props.message}
-            <div>
-            {props.like} likes
+            <div >
+            <div onClick={()=>{setLike(like+1)}} className={style.like}></div>
+                {like}
             </div>
 
         </div>
