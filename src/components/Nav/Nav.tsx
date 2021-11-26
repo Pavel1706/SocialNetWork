15966b8998bc48111918b3ab4a92ba0a377e2d47
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-11-27 00:59:27 +0300

    feat: change style Nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,40 +1,38 @@
 import React from 'react';
-import p from './Nav.module.css';
+import style from './Nav.module.css';
 import {NavLink} from "react-router-dom";
 import {ConversationType} from "../../Redux/dialogsReducer";
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../Redux/reduxStore";
 
 
 
+export const Nav = () => {
+    let dialogsPage = useSelector<AppStateType,ConversationType[]>(state => state.dialogsPage.dialogs)
 
-
-type ConversationArrayType = {
-    dialogs: ConversationType[]
-}
-
-export const Nav = (props: ConversationArrayType) => {
-    let fotka = props.dialogs.map(t=> <img key={t.id} className={p.avatar} src={t.foto}/>)
+    let fotka = dialogsPage.map(t=> <img key={t.id} className={style.avatar} src={t.foto}/>)
     return (
-        <nav className={p.nav}>
-            <div className={`${p.sentence} ${p.active}`}>
-                <NavLink to="/profile" activeClassName={p.activeLink}>Profile</NavLink>
+        <nav className={style.nav}>
+            <div className={`${style.sentence} ${style.active}`}>
+                <NavLink to="/profile" activeClassName={style.activeLink}>Profile</NavLink>
             </div>
-            <div className={p.sentence}>
-                <NavLink to="/conversation" activeClassName={p.activeLink}>Messages</NavLink>
+            <div className={style.sentence}>
+                <NavLink to="/conversation" activeClassName={style.activeLink}>Messages</NavLink>
             </div>
-            <div className={p.sentence}>
-                <NavLink to="/news" activeClassName={p.activeLink}>News</NavLink>
+            <div className={style.sentence}>
+                <NavLink to="/news" activeClassName={style.activeLink}>News</NavLink>
             </div>
-            <div className={p.sentence}>
-                <NavLink to="/music" activeClassName={p.activeLink}>Music</NavLink>
+            <div className={style.sentence}>
+                <NavLink to="/music" activeClassName={style.activeLink}>Music</NavLink>
             </div>
-            <div className={p.sentence}>
-                <NavLink to="/settings" activeClassName={p.activeLink}>Settings</NavLink>
+            <div className={style.sentence}>
+                <NavLink to="/settings" activeClassName={style.activeLink}>Settings</NavLink>
             </div>
-            <div className={p.sentence}>
-                <NavLink to="/users" activeClassName={p.activeLink}>Users</NavLink>
+            <div className={style.sentence}>
+                <NavLink to="/users" activeClassName={style.activeLink}>Users</NavLink>
             </div>
-            <div className={p.sentence}>
-                <NavLink to="/Friends" className={p.avatar}>Friends</NavLink>
+            <div className={style.sentence}>
+                <NavLink to="/Friends" className={style.avatar}>Friends</NavLink>
                 <div>{fotka}</div>
             </div>
         </nav>
