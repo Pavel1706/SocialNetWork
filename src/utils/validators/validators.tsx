fa72180a8f8e1628ce47b22ea55f364e12f2f22f
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-03 22:46:55 +0300

    feat: change validators.tsx

--- src/utils/validators/validators.tsx
+++ src/utils/validators/validators.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
+export type  FieldValidatorType = (value: number) => string | undefined
 
-export const requiredField = (value:string)=> {
+export const requiredField: FieldValidatorType = (value) => {
     if (value) return undefined;
     return 'Field is required'
 }
 
 
-
-export const maxLengthCreator =(maxLength: number) => (value: string) => {
-    if (value && value.length > maxLength) return `Max length is ${maxLength} symbols`;
+export const maxLengthCreator = (maxLength: number) => (value: string) => {
+    if (value.length > maxLength) return `Max length is ${maxLength} symbols`;
     return undefined
 }
