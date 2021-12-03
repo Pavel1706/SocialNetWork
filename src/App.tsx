a730e16d6a0dbfe6e2e98535cee23a78c3ba555d
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-03 22:47:15 +0300

    feat: change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
-import React, {useEffect} from 'react';
+import React, {ComponentType, useEffect} from 'react';
 import './App.css';
 import {Nav} from "./components/Nav/Nav";
-import {Conversation} from "./components/Conversation/Conversation";
-import {BrowserRouter, Route} from "react-router-dom";
+// import {Conversation} from "./components/Conversation/Conversation";
+import {BrowserRouter, HashRouter, Route} from "react-router-dom";
 import {Friends} from "./components/Friends/Friends";
-// import {store} from "./Redux/State";
 import HeaderContainer from './components/Header/HeaderContainer';
 import UsersContainer from "./components/Users/UsersContainer";
 import Login from "./components/LoginComponent/Login";
-import ProfileContainer from "./components/Profile/ProfileContainer";
+// import ProfileContainer from "./components/Profile/ProfileContainer";
 import {Provider, useDispatch, useSelector} from 'react-redux';
 import {initializeTC} from "./Redux/appReducer";
-import {AppStateType} from "./Redux/reduxStore";
+import {AppStateType, store} from "./Redux/reduxStore";
 import {Preloader} from "./components/common/Preloader/Preloader";
-import {store} from "./Redux/reduxStore";
-import {ConversationType} from "./Redux/dialogsReducer";
+import {withSuspense} from "./Hoc/withSuspense";
+import {Switch} from "@mui/material";
 
 
+const Conversation = React.lazy(()=> import ('./components/Conversation/Conversation')
+    .then(({Conversation})=>({default:Conversation}))
+);
 
+const ProfileContainer = React.lazy(()=> import ('./components/Profile/ProfileContainer')
+   );
 
 
 export function AppContainer() {
     let initialized = useSelector<AppStateType>(state => state.app.initialized)
-    let dialogsPage = useSelector<AppStateType,ConversationType[]>(state => state.dialogsPage.dialogs)
+
 
 
     const dispatch = useDispatch()
@@ -35,28 +39,38 @@ export function AppContainer() {
         return (
             <div className='app-wrapper'>
                 <HeaderContainer/>
-                <Nav dialogs={dialogsPage}/>
+                <Nav />
                 <div className='app-wrapper-content '>
-                    <Route path='/conversation' render={() => <Conversation/>}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+
+                    <Route path='/conversation' render={withSuspense(Conversation)
+                        // () => {
+                            //     return <React.Suspense fallback={<Preloader loading={true}/>}>
+                            //         <Conversation/>
+                            //     </React.Suspense>
+                            // }
+                        // }
+                    }/>
+                    <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)
+                    }/>
                     <Route path='/users' render={() => <UsersContainer/>}/>
-                    <Route path='/login' render={() => <Login/>}/>
+                    <Route exact path='/login' render={() => <Login/>}/>
                     <Route path='/news'/>
                     <Route path='/music'/>
                     <Route path='/settings'/>
-                    <Route path='/friends' render={() => <Friends dialogs={dialogsPage}/>}/>
+                    <Route path='/friends' render={() => <Friends />}/>
                 </div>
             </div>
 
         );
-    }
+}
 
-    export const SamuraiJSApp=()=>{
-    return  <BrowserRouter>
+
+export const SamuraiJSApp=()=>{
+    return  <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
-    </BrowserRouter>
-    }
+    </HashRouter>
+}
 
 
