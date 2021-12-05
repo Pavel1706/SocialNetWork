afdc5b0dc7d935daf0a45049d7048f1b54f0620b
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-05 20:44:04 +0300

    feat: change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 import './index.css';
 import ReactDOM from 'react-dom';
 import {SamuraiJSApp} from "./App";
-//test commit
+
+
 ReactDOM.render(
     <SamuraiJSApp/>,
     document.getElementById('root')
 )
 
 
-
-// <BrowserRouter>
-// <Provider store={store}>
-//     <App/>
-//     </Provider>
-// </BrowserRouter>,
