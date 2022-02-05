97d5fe5cd12c16db61a50d5ba32fc046b2f1b603
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2022-02-05 21:33:52 +0300

    feat: deleted comment

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -34,42 +34,7 @@ export let Users = (props: UsersStateType) => {
                                              followUserTC={props.followUserTC}
                                              unFollowUserTC={props.unFollowUserTC}   />)
 
-                // <span>
-                //     <div>
-                //         <NavLink to={'/profile/' + t.id}>
-                //         <img
-                //             src={t.photos.small != null ? t.photos.small : 'https://i.pinimg.com/736x/9b/80/f0/9b80f06c91e4c03c63059d35ff943168.jpg'}
-                //             className={styles.photo}/>
-                //             </NavLink>
-                //     </div>
-                //     <div>
-                //         {
-                //             t.followed
-                //                 ?
-                //                 <button disabled={props.followingInProgress.some(id=>id===t.id)} onClick={() => {
-                //
-                //                     props.setToggleIsFollowing(true,t.id)
-                //                       props.unFollowUserTC(t.id)
-                //
-                //
-                //                 }}>UnFollow</button>
-                //                 :
-                //                 <button disabled={props.followingInProgress.some(id=>id===t.id)} onClick={() => {
-                //
-                //                     props.setToggleIsFollowing(true,t.id)
-                //                     props.followUserTC(t.id)
-                //
-                //                 }}>Follow</button>
-                //
-                //         }
-                //     </div>
-                // </span>
-                // <span>
-                //     <span>
-                //         <div>{t.name}</div>
-                //         <div>{t.status}</div>
-                //     </span>
-                // </span>
+        
 
         }
         <Paginator pageSize={props.pageSize}
